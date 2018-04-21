85f4c8ad8fad60dfb0071e2093c852af8c8c98fe
Yan Jardim <yan.guilherme72@gmail.com>
2018-04-21 15:32:50 -0300

    - Add enemy attack

---
+++ Assets/Scripts/Agents/Enemy.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : Actor
+{
+
+    public Vector2 CooldownRange;
+    private bool _canPlay;
+    // Use this for initialization
+    void Start()
+    {
+        _canPlay = false;
+        TurnController.OnTurnBegin += OnTurnBegin;
+    }
+
+    // Update is called once per frame
+    void OnTurnBegin(Actor target)
+    {
+        if (!IsMyTurn) return;
+
+        StartPlayCoroutine();
+    }
+    void Play()
+    {
+        Debug.Log("Enemy Attack!");
+        GameController.Instance.TurnController.ChangeTurn();
+    }
+
+    void StartPlayCoroutine()
+    {
+        StartCoroutine(PlayAfterRandomTimeCoroutine());
+    }
+
+    IEnumerator PlayAfterRandomTimeCoroutine()
+    {
+
+        yield return new WaitForSeconds(Random.Range(CooldownRange.x, CooldownRange.y));
+        Play();
+    }
+}
